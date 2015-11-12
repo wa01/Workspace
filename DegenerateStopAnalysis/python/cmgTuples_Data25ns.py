96209b2b5644e3b589aad3534d8c371128353d12
Navid Rad <navid.rad@cern.ch>
2015-11-12 10:43:12 +0100

    sample update

---
+++ DegenerateStopAnalysis/python/cmgTuples_Data25ns.py
@@ -0,0 +1,65 @@
+data_path = "/data/nrad/cmgTuples/RunII/7412pass2/Data25ns" 
+
+SingleMuon_Run2015D_v4 ={\
+"chunkString":"",
+"name" : "SingleMuon_Run2015D-PromptReco-v4_Data25ns",
+"dir": data_path,
+"rootFileLocation":"/tree.root",
+"treeName":"tree",
+'isData':True,
+}
+
+SingleMuon_Run2015D_05Oct ={\
+"chunkString":"",
+"name" : "SingleMuon_Run2015D-05Oct2015-v1_Data25ns",
+"dir": data_path,
+"rootFileLocation":"/tree.root",
+"treeName":"tree",
+'isData':True,
+}
+
+SingleElectron_Run2015D_v4 ={\
+"chunkString":"",
+"name" : "SingleElectron_Run2015D-PromptReco-v4_Data25ns",
+"dir": data_path,
+"rootFileLocation":"/tree.root",
+"treeName":"tree",
+'isData':True,
+}
+SingleElectron_Run2015D_05Oct ={\
+"chunkString":"",
+"name" : "SingleElectron_Run2015D-05Oct2015-v1_Data25ns",
+"dir": data_path,
+"rootFileLocation":"/tree.root",
+"treeName":"tree",
+'isData':True,
+}
+MET_Run2015D_v4 ={\
+"chunkString":"",
+"name" : "MET_Run2015D-PromptReco-v4_Data25ns",
+"dir": data_path,
+"rootFileLocation":"/tree.root",
+"treeName":"tree",
+'isData':True,
+}
+MET_Run2015D_05Oct ={\
+"chunkString":"",
+"name" : "MET_Run2015D-05Oct2015-v1_Data25ns",
+"dir": data_path,
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':True,
+}
+
+
+samples = [MET_Run2015D_05Oct, MET_Run2015D_v4, SingleElectron_Run2015D_05Oct, SingleElectron_Run2015D_v4, SingleMuon_Run2015D_05Oct, SingleMuon_Run2015D_v4]
+
+for sample in samples:
+  sample['dir'] = sample['dir']+"/"+sample['name']
+
+
+
+
+
+
+
